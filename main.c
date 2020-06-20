47eef78e20470126bd1a819eeb2b2a77af15803d
Rodro560 <67166664+Rodro560@users.noreply.github.com>
2020-06-19 22:29:50 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -6,11 +7,78 @@
 int main()
 {
     srand(time(0));
-    //todo lo del main es de prueba por ahora, falta el menú
-    int carton1[3][5];
+    int cantCarton, tipoCarton, carton1[3][5],carton2[3][5],carton3[3][5];
 
-    asignarCarton(carton1);
-    dibujarCarton(carton1, 1);
+    do{
+            //las siguentes lineas son para elegir la cantidad de cartones
+        printf("Bienvenido al bingo\n\n");
+        printf("Escribe 1 para solo usar un carton\n");
+        printf("Escribe 2 para solo usar dos cartones\n");
+        printf("Escribe 3 para solo usar tres cartones\n");
+        printf("Una ves formado tu carton presiona 0\n");
+        scanf("%d",&cantCarton);
+
+        //las siguentes lineas son para elegir como van a cargar el carton
+    printf("\nQue carton quieres?\n");
+    printf("\nIngrese 1 para tomar un/unos carton/cartones alatorio/s\n");
+    printf("Ingrese 2 para personalizar su/s carton/cartones\n");
+    scanf("%d",&tipoCarton);
+
+        if(tipoCarton==1){
+
+            switch(cantCarton){
+
+                case 1:asignarCarton(carton1);
+               dibujarCarton(carton1, 1);break;
+
+               case 2:asignarCarton(carton1);
+               dibujarCarton(carton1, 1);
+
+               asignarCarton(carton2);
+               dibujarCarton(carton2, 1);break;
+
+               case 3:asignarCarton(carton1);
+               dibujarCarton(carton1, 1);
+
+               asignarCarton(carton2);
+               dibujarCarton(carton2, 1);
+
+               asignarCarton(carton3);
+               dibujarCarton(carton3, 1);break;
+
+               case 0:break;
+
+               default:printf("\n------error------\n");
+            }
+        }
+        if(tipoCarton==2){
+
+               switch(cantCarton){
+
+                case 1:asignarCarton(carton1);
+               dibujarCarton(carton1, 1);break;
+
+               case 2:asignarCarton(carton1);
+               dibujarCarton(carton1, 1);
+
+               asignarCarton(carton2);
+               dibujarCarton(carton2, 1);break;
+
+               case 3:asignarCarton(carton1);
+               dibujarCarton(carton1, 1);
+
+               asignarCarton(carton2);
+               dibujarCarton(carton2, 1);
+
+               asignarCarton(carton3);
+               dibujarCarton(carton3, 1);break;
+
+               case 0:break;
+
+               default:printf("\n------error------\n");
+            }
+        }
+    }while(cantCarton==0);
 
     return 0;
 }
